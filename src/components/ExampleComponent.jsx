fd0169e779567faae4fc3ff86de0f2a3b7c20f79
Seba <sebastiantorresola3@gmail.com>
2024-04-11 11:23:42 -0700

    Removed unnecessary Vite boilerplate and made components directory

---
+++ src/components/ExampleComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ExampleComponent = () => {
+  return (
+    <div>Delete this component after we have made our first component!</div>
+  )
+}
+
+export default ExampleComponent
