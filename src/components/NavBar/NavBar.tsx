b67813ee2759fb88085afdcc0e288cbe6490bdde
ruru-dev <rurutia89@gmail.com>
2024-05-03 01:24:47 -0500

    Show nav items based on user authentication state

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,13 +1,19 @@
+import { useSelector } from "react-redux";
+import { AppState } from "../../redux/Store";
 import NavItems from "./NavItems";
 import Logo from "./Logo";
 
 import "./NavBar.css";
 
 export default function NavBar(): JSX.Element {
-  return(
-  <div className="container-nav-bar">
-    <Logo />
-    <NavItems />
-  </div>
+  const isLoggedIn: boolean = useSelector(
+    (state: AppState) => state.user.isLoggedIn
+  );
+
+  return (
+    <div className="container-nav-bar">
+      <Logo />
+      {isLoggedIn && <NavItems />}
+    </div>
   );
 }
