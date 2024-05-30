773ebb8173d3e6c15feaeb6ad77cf6a1f3ee601d
PeytenBevill <redsage00@gmail.com>
2024-05-30 17:16:08 -0500

    added profile quick view interface

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { ProfileQuickView } from "../ProfileQuickView/ProfileQuickView";
 import "./Home.css";
+import { bgUri, pfpUri } from "./mockData";
 
 const Home: React.FC = () => {
   return (
@@ -8,7 +9,17 @@ const Home: React.FC = () => {
       <div className="feed"> test</div>
 
       <div className="quickView">
-        <ProfileQuickView />
+        <ProfileQuickView
+          name="Chris Hemsworth"
+          position="HR Specialist"
+          userName="@ChrisH_72"
+          endorsements={15}
+          bio="A budding HR specialist looking for a small to medium sized company!"
+          skills={["test"]}
+          badge="test"
+          pfp={pfpUri}
+          bgPic={bgUri}
+        />
       </div>
     </div>
   );
